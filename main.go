8e4d2b066c64e5b48ec41e9ccbb770d0998b5e0d
panelenelo <panelenelo@gmail.com>
2026-05-04 18:30:58 -0300

    new routes

--- main.go
+++ main.go
@@ -1,31 +1,40 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
+	"strconv"
 )
 
-var token string
-var randomGen *rand.Rand
+const port string = ":8181"
 
-func random(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Authorization") != "Bearer "+token {
-		w.WriteHeader(http.StatusUnauthorized)
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hilòu"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(map[string]int{
-		"value": randomGen.Intn(100)})
+	var message string = ("Viewing the snippet: " + strconv.Itoa(id))
+	w.Write([]byte(message))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Form for creating a snippet"))
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "hiilóu")
-		//io.WriteString(w, "hilöu\n")
-	})
-	fmt.Println("server running on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view/{id}", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	fmt.Println("server running on localhost", port)
+	err := http.ListenAndServe(port, mux)
+	log.Fatal(err)
 }
