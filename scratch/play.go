13c4f2b52235afd92cef658b8a7ab207d294ce84
panelenelo <panelenelo@gmail.com>
2026-05-30 20:45:48 -0300

    gif test

--- scratch/play.go
+++ scratch/play.go
@@ -3,6 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
+	"math"
+	"math/rand/v2"
+	"os"
 )
 
 func test1() {
@@ -122,21 +129,6 @@ type testc struct {
 	stg string
 }
 
-func main() {
-
-	var yp []testc
-
-	for i := 0; i <= 3; i++ {
-		var vi testc
-		vi.num = 2
-		vi.stg = "mango"
-		yp = append(yp, vi)
-	}
-
-	fmt.Print(yp)
-
-}
-
 func parseFlags() (string, string) {
 	addr := flag.String("addr", "portMango", "HTTP network address")
 
@@ -151,4 +143,48 @@ func parseFlags() (string, string) {
 	*addr = fmt.Sprintf(":%s", *addr)
 
 	return dsn, *addr
+}
+
+var palette = []color.Color{color.White, color.Black}
+
+const (
+	whiteIndex = 0 // first color in palette
+	blackIndex = 1 // next color in palette
+)
+
+func main() {
+	lissajous(os.Stdout)
+}
+
+// Have to build to run this function, and then direct the result to a file
+// ex: ./play > out.gif
+func lissajous(out io.Writer) {
+	const (
+		cycles = 5
+		// number of complete x oscillator revolutions
+		res  = 0.001 // angular resolution
+		size = 100
+		// image canvas covers [-size..+size]
+		nframes = 64
+		// number of animation frames
+		delay = 8
+	// delay between frames in 10ms units
+	)
+	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // phase difference
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
+				blackIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
 }
