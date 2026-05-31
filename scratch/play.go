0b1c5b17948d5933fee1c8fc57271d1c526f6661
panelenelo <panelenelo@gmail.com>
2026-05-31 16:58:00 -0300

    test goroutine

--- scratch/play.go
+++ scratch/play.go
@@ -9,7 +9,7 @@ import (
 	"io"
 	"math"
 	"math/rand/v2"
-	"os"
+	"time"
 )
 
 func test1() {
@@ -153,7 +153,26 @@ const (
 )
 
 func main() {
-	lissajous(os.Stdout)
+	go spinner(100 * time.Millisecond)
+	const n = 45
+	fibN := fib(n) // slow
+	fmt.Printf("\rFibonacci(%d) = %d\n", n, fibN)
+}
+
+func spinner(delay time.Duration) {
+	for {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+		}
+	}
+}
+
+func fib(x int) int {
+	if x < 2 {
+		return x
+	}
+	return fib(x-1) + fib(x-2)
 }
 
 // Have to build to run this function, and then direct the result to a file
